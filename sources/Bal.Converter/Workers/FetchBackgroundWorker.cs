2026c8c07b0b8212a5237451117b3ac519104f06
bozali <alibozna@gmail.com>
2023-01-18 18:33:14 +0100

    Add fetch background worker

---
+++ sources/Bal.Converter/Workers/FetchBackgroundWorker.cs
@@ -0,0 +1,21 @@
+﻿using Bal.Converter.Services;
+
+namespace Bal.Converter.Workers;
+
+public class FetchBackgroundWorker
+{
+    private readonly IDownloadsRegistryService downloadsRegistry;
+
+    public FetchBackgroundWorker(IDownloadsRegistryService downloadsRegistry)
+    {
+        this.downloadsRegistry = downloadsRegistry;
+    }
+
+    public async Task Process(CancellationToken ct = default)
+    {
+        while (!ct.IsCancellationRequested)
+        {
+            await this.downloadsRegistry.GetDownloadJob();
+        }
+    }
+}
